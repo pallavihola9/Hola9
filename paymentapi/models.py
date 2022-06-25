e717d67cea455016ce2aa2d912fa742f82bc5f52
sanjay <sanjaysaha1120@gmail.com>
2022-06-25 16:51:41 +0530

    Changes on 25.06.2022

--- paymentapi/models.py
+++ paymentapi/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+
+from adsapi.models import Product
+from account.models import User
 # Create your models here.
 
 class Order(models.Model):
@@ -10,3 +13,12 @@ class Order(models.Model):
 
     def __str__(self):
         return self.product_name
+
+class TransationIdone(models.Model):
+    id1=models.CharField(max_length=2322)
+    dateid=models.CharField(max_length=232)
+    message=models.CharField(max_length=232)
+    adsid=models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    userid=models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    ProductData=models.CharField(max_length=43343333,null=True)
+
